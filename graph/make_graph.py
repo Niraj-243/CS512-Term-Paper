bbce88a69bda554c3a17b363dc9d6d2bde4c0398
Niraj Kumar <2020mcb1243@iitrpr.ac.in>
2023-10-13 15:42:21 +0530

    remove print lines

--- graph/make_graph.py
+++ graph/make_graph.py
@@ -1,4 +1,4 @@
-def make_graph(clusters,source,dest,rect_shape):
+def make_graph(clusters,source,dest):
     x0,y0 = source
     xn,yn = dest
     
@@ -11,11 +11,9 @@ def make_graph(clusters,source,dest,rect_shape):
         x0,y0 = q.pop(0)
         for i,j,k in clusters:
             if i <= y0 or i >= yn: continue
-            print(i,j,k)
             m = (x0-xn)*(1.0)/(y0-yn)
             c1 = x0*(1.0) + m*(i-y0) - j
             c2 = x0*(1.0) + m*(i-y0) - k
-            print(x0,m,i,y0,j,k,c1,c2)
             if c1*c2 <0 :
                 dy = i-1
                 if j-1>=0:
