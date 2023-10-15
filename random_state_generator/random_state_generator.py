3fa9ded02406db49b234afa1dfaa55687519cb43
Niraj Kumar <2020mcb1243@iitrpr.ac.in>
2023-10-15 16:43:16 +0530

    modify random state generator

--- random_state_generator/random_state_generator.py
+++ random_state_generator/random_state_generator.py
@@ -12,7 +12,8 @@ def random_state_generator(length,height,num):
     for i,xp in enumerate(x_pts):
         for j in range(y_pts[i][0],y_pts[i][1]+1):
             arr[j][xp] = 1
-    x_source,x_dest = None,None
+    x_source,x_dest = 0,length-1
+    y_source,y_dest = height-1,0
     while 1:
         x_source = random.choice(range(0,length))
         if x_source not in x_pts:
@@ -22,7 +23,6 @@ def random_state_generator(length,height,num):
         if x_dest not in x_pts and x_dest > x_source:
             break
     y_source,y_dest = random.choice((range(0,height))),random.choice((range(0,height)))
-    
     arr[y_source][x_source] = 2 
     arr[y_dest][x_dest] = 3
     return arr,(y_source,x_source),(y_dest,x_dest)
